4d04573aa23c10e2a4ad1e0965db1fa7ab0fec55
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-03-24 11:50:34 -0300

    feat : Adicionando nbvo produto ao array principal.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

--- estoque/js/script.js
+++ estoque/js/script.js
@@ -14,6 +14,33 @@ const btnAdicionar = document.getElementById("btn-adicionar");
 btnAdicionar.addEventListener("click", () => {
     const prodIdInput = document.getElementById("prod-id");
     const nomeInput = document.getElementById("prod-nome");
-    const precoInput = parseFloat(document.getElementById("prod-preco").value);
+    
+    const precoInput = document.getElementById("prod-preco");
+    precoInput.value = parseFloat(precoInput.value);
+
     const categoriaInput = document.getElementById("prod-categoria");
+
+    if (!prodIdInput.value || !nomeInput.value || isNaN(precoInput.value) || !categoriaInput.value) {
+        alert("Por favor, preencha todos os campos corretamente.");
+        return;
+    }
+    const novoProduto = {
+        id: parseInt(prodIdInput.value),
+        nome: nomeInput.value,
+        preco: precoInput.value,
+        categoria: categoriaInput.value,
+    };
+    estoque.push(novoProduto);
+    alert(`Produto "${novoProduto.nome}" adicionado com sucesso!`);
+
+
+    // Limpar os campos após adicionar
+    prodIdInput.value = "";
+    nomeInput.value = "";
+    precoInput.value = "";
+    categoriaInput.value = "";
+
+    console.log("Produto adicionado:", novoProduto);
+    console.log("Estoque atualizado:", estoque);
+
 });
